f1ebd77f77f29ed1962d5275c81765db40d6ba12
Amit Rajwar <amitxrajwar@gmail.com>
2026-03-17 22:44:41 -0700

    first commit

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const API_URL = import.meta.env.VITE_API_URL || '';
+
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ? `${import.meta.env.VITE_API_URL}/api` : '/api',
+  baseURL: API_URL ? `${API_URL}/api` : '/api',
 });
 
 API.interceptors.request.use((config) => {
